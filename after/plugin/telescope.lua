c45a9bb72e4e7255d81498782bdae41dab196102
Wilson Neto <wilsonpsn@hotmail.com>
2023-04-05 22:10:16 -0300

    feat(changes): do some changes
    Some changes that even I am unsure about

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>pf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', function() builtin.live_grep({grep_open_files=true}) end)
 vim.keymap.set('n', '<leader>ps',  function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") } );
 end)
