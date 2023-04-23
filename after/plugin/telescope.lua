2b149f22e7089503a18b440676de5b5ffd174a3d
Wilson Neto <wilsonpsn@hotmail.com>
2023-04-22 23:37:58 -0300

    feat(telescope): upgrade keybinds
    This upgrade the keybind for live grep and add a new keybind for buffer
    search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>pf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', function() builtin.live_grep({grep_open_files=true}) end)
+vim.keymap.set('n', '<leader>pg', function() builtin.live_grep({grep_open_files=true}) end)
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps',  function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") } );
 end)
