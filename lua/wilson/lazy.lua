b3ce3bc6afec057575452e88febd71fa57b9a33f
Wilson Neto <wilsonpsn@hotmail.com>
2025-06-09 18:21:32 -0300

    update default model to gpt 4.1

--- lua/wilson/lazy.lua
+++ lua/wilson/lazy.lua
@@ -52,7 +52,7 @@ require("lazy").setup({
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
 		debug = true, -- Enable debugging
-		model = "gpt-4o",
+		model = "gpt-4.1",
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
