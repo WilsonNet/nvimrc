7e8e2cf530c1d52eaf512ff4e37894cf6e35e90d
Wilson Neto <wilsonpsn@hotmail.com>
2025-12-15 11:14:44 -0300

    haha

--- lua/wilson/lazy/vimwiki.lua
+++ lua/wilson/lazy/vimwiki.lua
@@ -1,16 +1,18 @@
 return {
-  'vimwiki/vimwiki',
-  config = function()
-    -- Configure vimwiki to use markdown in ~/notes
-    vim.g.vimwiki_list = {
-      {
-        path = '~/notes',
-        syntax = 'markdown',
-        ext = '.md',
-        diary_ext = '.md'
-      }
-    }
-    -- Map Ctrl+Space to toggle checkbox
-    vim.keymap.set('n', '<C-Space>', '<Plug>VimwikiToggleListItem')
-  end
+	'vimwiki/vimwiki',
+	config = function()
+		-- Configure vimwiki to use markdown in ~/notes
+		vim.g.vimwiki_list = {
+			{
+				path = vim.fn.expand('~/notes'),
+				syntax = 'markdown',
+				ext = '.md',
+				diary_ext = '.md'
+			}
+		}
+		vim.g.vimwiki_global_ext = 0
+		vim.g.vimwiki_ext2syntax = {}
+		-- Map Ctrl+Space to toggle checkbox
+		vim.keymap.set('n', '<C-Space>', '<Plug>VimwikiToggleListItem')
+	end
 }
